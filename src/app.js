3b7f23316b360b431e2c750bf8d347c0d9a78564
Kübra Özgan <86002359+KubraOzgan@users.noreply.github.com>
2022-01-19 00:16:14 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ events();
 const app = express();
 app.use(express.json());
 
+app.use("/user-images", express.static(path.join(__dirname, "./", "uploads/users")));
+app.use("/tweet-images", express.static(path.join(__dirname, "./", "uploads/tweets"))); 
+
 app.listen(process.env.APP_PORT, () => {
     console.log(`Application is running on ${process.env.APP_PORT}`);
     app.use("/users", UserRoutes);
